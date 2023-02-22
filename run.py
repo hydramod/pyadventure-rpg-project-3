dea2ec9d1b9a01bc290cc3644e31c342927f5733
Ali <alissaeid@live.co.uk>
2023-02-22 20:24:51 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import cmd
+import textwrap
+import sys
+import os
+import time
+import random
+
+
+class player_setup:
+    """
+    Initial player setup, health points, mana points and status effects
+    """
+    def __init__(self):
+        self.name = ""
+        self.health = 0
+        self.mana = 0
+        self.status = []
+
+
+player = player_setup()
+
+def title_menu():
+    """
+    Controls the title menu screen options
+    """
+    option = input("-> ")
+    
