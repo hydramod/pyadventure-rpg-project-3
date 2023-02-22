d9ae8c4b94b0b93d6a5213e8aadd1ea27e7216a7
Ali <alissaeid@live.co.uk>
2023-02-22 22:05:35 +0000

    Add function for title menu, help menu, initial player setup class and main function

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ class player_setup:
     """
     Initial player setup, health points, mana points and status effects
     """
+
     def __init__(self):
         self.name = ""
         self.health = 0
@@ -19,9 +20,53 @@ class player_setup:
 
 player = player_setup()
 
+
 def title_menu():
     """
     Controls the title menu screen options
     """
-    option = input("-> ")
-    
+    options = {
+        "play": start,
+        "help": help,
+        "quit": sys.exit,
+    }
+
+    while True:
+        option = input("-> ").lower()
+        if option in options:
+            options[option]()
+        else:
+            print("Invalid command!\nEnter a valid command:")
+
+
+def title_screen():
+    """
+    Prints title screen
+    """
+    os.system("clear")
+    print("*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+    print("*#   WELCOME TO PYADVENTURE RPG   #*")
+    print("*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+    print("             <- Play ->             ")
+    print("             <- Help ->             ")
+    print("             <- Quit ->             ")
+    print("      Copyright 2023 Ali Saeid      ")
+    title_menu()
+
+
+def help_menu():
+    print("*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*")
+    print("*#     WELCOME TO PYADVENTURE RPG    #*")
+    print("*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*")
+    print("<- Use UP, DOWN, LEFT, RIGHT to move ->")
+    print("<-   Type your commands to do them   ->")
+    print("<-  Use examine to inspect something ->")
+    print("<-      Have fun and good luck       ->")
+    title_menu()
+
+
+def main():
+    """
+    Runs game functions
+    """
+    title_screen()
